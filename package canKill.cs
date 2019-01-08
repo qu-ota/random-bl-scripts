27afdeea95b972f0469b7909c1eefda8dfdd78f0
dom <34490428+qu-ota@users.noreply.github.com>
2019-01-08 09:00:14 -0500

    Update package canKill.cs

--- package canKill.cs
+++ package canKill.cs
@@ -1,10 +1,10 @@
 package canKill
 {
-    function minigameCanDamage(%a, %b)
-    {
-        if (%a.canKill)
-            return 1;
-        return parent::minigameCanDamage(%a, %b);
-    }
+	function minigameCanDamage(%a, %b)
+	{
+		if (%a.canKill)
+			return 1;
+		return parent::minigameCanDamage(%a, %b);
+	}
 };
 activatePackage(canKill);
